38bcc374ad78f99db9ec9e3a18895c73d67e376c
iggyharapeko <iggyharapeko@gmail.com>
2025-05-31 22:58:35 +0800

    fixpls

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
     };
     return config;
   },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 };
 
 module.exports = nextConfig; 
